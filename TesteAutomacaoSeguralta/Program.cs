e262f3514e8207245649a4e8c2cf049153c5cb34
italosanches <51172478+italosanches@users.noreply.github.com>
2019-05-30 15:13:43 -0300

    Add files via upload

--- TesteAutomacaoSeguralta/Program.cs
+++ TesteAutomacaoSeguralta/Program.cs
@@ -12,20 +12,21 @@ namespace TesteAutomacaoSeguralta
 {
     class Program
     {
-        ContextDB dataBase = new ContextDB("SUA CONNECTION STRING AQUI");
+        ContextDB dataBase = new ContextDB("Data Source=(local);Initial Catalog=seguralta;Integrated Security=True");
 
         static void Main(string[] args)
         {
-            string nomeCompleto = "Alexandre Videschi Marques";
-            string email = "alexandre.ti@seguralta.com.br";
-            int CEP = 15015700;
-            string estado = "São Paulo";
-            string cidade = "São José do Rio Preto";
-            string assunto = "Teste projeto Seguralta";
-            string telefone = "017997777777";
-            string mensagem = "Teste do projeto da Seguralta";
+            /*
+             string nomeCompleto = "Alexandre Videschi Marques";
+             string email = "alexandre.ti@seguralta.com.br";
+             int CEP = 15015700;
+             string estado = "São Paulo";
+             string cidade = "São José do Rio Preto";
+             string assunto = "Teste projeto Seguralta";
+             string telefone = "017997777777";
+             string mensagem = "Teste do projeto da Seguralta";*/
 
-            //Codificar aqui e excluir essa linha para não interferir no teste.
+             //Codificar aqui e excluir essa linha para não interferir no teste.
             AutomatizarGrid(); //Linha de exemplo
         }
 
@@ -45,23 +46,33 @@ namespace TesteAutomacaoSeguralta
             driver.Manage().Window.Maximize();
 
             //Navegando para o site
-            driver.Navigate().GoToUrl("https://codepen.io/koalyptus/embed/wKBOLp?height=446&theme-id=0&slug-hash=wKBOLp&default-tab=result&user=koalyptus&name=cp_embed_1");
+            driver.Navigate().GoToUrl("http://seguralta.com.br/site/contato");
 
             //Trocando o contexto para o IFrame dos elementos para interação, no exemplo do formulário da Seguralta não será necessário
-            driver.SwitchTo().Frame(0);
+            //driver.SwitchTo().Frame(0);
 
             //Definindo os valores
 
-            driver.FindElement(By.Id("flt0_demo")).SendKeys("Benin");
-            new SelectElement(driver.FindElement(By.CssSelector("#demo > thead > tr.fltrow > td:nth-child(4) > select"))).SelectByValue(">0 && <=25000");
-            driver.FindElement(By.Id("flt5_demo")).SendKeys("190");
-            driver.FindElement(By.Id("flt5_demo")).SendKeys(Keys.Enter);
+            driver.FindElement(By.Id("name")).SendKeys("Alexandre Videschi Marques");
+            driver.FindElement(By.Id("email")).SendKeys("alexandre.ti@seguralta.com.br");
+            System.Threading.Thread.Sleep(5000);
+            driver.FindElement(By.Id("cep")).SendKeys("15015700");
+            driver.FindElement(By.Id("estado")).SendKeys("São Paulo");
+            System.Threading.Thread.Sleep(5000);
+            driver.FindElement(By.Id("cidade")).SendKeys("São José do Rio Preto");
+            driver.FindElement(By.Id("assunto")).SendKeys("Teste projeto Seguralta");
+            driver.FindElement(By.Id("mensagem")).SendKeys("y gybyybuyg ygygygyig gyyiuf y ");
+            driver.FindElement(By.Id("telefone")).SendKeys("017997777777");
+            //driver.FindElement(By.ClassName("btn btn-success btn-orange btn-md pull-right")).Click();
+            //new SelectElement(driver.FindElement(By.CssSelector("#demo > thead > tr.fltrow > td:nth-child(4) > select"))).SelectByValue(">0 && <=25000");
+            // driver.FindElement(By.Id("flt5_demo")).SendKeys("190");
+            //driver.FindElement(By.Id("flt5_demo")).SendKeys(Keys.Enter);
 
             //Obtendo valor
-            string year = driver.FindElement(By.CssSelector("#demo > tbody > tr.odd > td:nth-child(3)")).Text;
+            //string year = driver.FindElement(By.CssSelector("#demo > tbody > tr.odd > td:nth-child(3)")).Text;
 
             //Printando valor
-            Console.WriteLine("O ano é: " + year);
+            // Console.WriteLine("O ano é: " + year);
         }
     }
 }
